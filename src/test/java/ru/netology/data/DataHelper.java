c225bbbb11ab4a3283c5d5e5c9eab5cef2566cc6
Kristina Uldina <uldinakris@gmail.com>
2022-05-15 21:47:20 +0200

    create tests

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,26 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+    private DataHelper() {}
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() { return new AuthInfo("vasya", "querty123"); }
+
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) { return new AuthInfo("petya", "123querty"); }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+}
---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,17 @@
+package ru.netology.test;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPageV1;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV1();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verifyInfo = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verifyInfo);
+    }
+}
