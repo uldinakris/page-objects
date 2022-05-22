1b8ca2e2cfe4612a8177edfbca80e206e5669ac9
Kristina Uldina <uldinakris@gmail.com>
2022-05-22 18:50:33 +0200

    added tests

--- src/test/java/ru/netology/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -25,4 +25,38 @@ public class MoneyTransferTest {
 
         assertEquals(initialBalance + 500, balanceAfterTransfer);
     }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+        var loginPage = new LoginPageV1();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.authoriseUser(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validateUser(verificationCode);
+        var initialBalance = dashboardPage.getCardBalance("0002");
+        var transferMoneyPage = dashboardPage.transferMoney("0002");
+        var dashboardPage2 = transferMoneyPage.transferMoney("500", "5559 0000 0000 0001");
+        var balanceAfterTransfer = dashboardPage2.getCardBalance("0002");
+
+        assertEquals(initialBalance + 500, balanceAfterTransfer);
+    }
+
+    @Test
+    void shouldNotTransferMoneyBetweenTheSameCard() {
+        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
+        var loginPage = new LoginPageV1();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.authoriseUser(authInfo);
+        var verificationCode = DataHelper.getVerificationCode(authInfo);
+        var dashboardPage = verificationPage.validateUser(verificationCode);
+        var initialBalance = dashboardPage.getCardBalance("0002");
+        var transferMoneyPage = dashboardPage.transferMoney("0002");
+        var dashboardPage2 = transferMoneyPage.transferMoney("500", "5559 0000 0000 0002");
+        var balanceAfterTransfer = dashboardPage2.getCardBalance("0002");
+
+        assertEquals(initialBalance, balanceAfterTransfer);
+    }
 }
